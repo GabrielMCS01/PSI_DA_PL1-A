08e1f98ebdeb63db585dccddcd93f0daa910622e
Diogo Pereira <diogo.rc.pereira@gmail.com>
2021-05-04 14:56:33 +0100

    botão sair com mensagem

---
+++ app/GestaoCamaraMunicipal/GestaoCamaraMunicipal/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace GestaoCamaraMunicipal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestãoDePromotoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestãoDeProcessosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestãoDeProjetosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestãoDePareceresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestãoDeFuncionáriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Red;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(538, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestãoDePromotoresToolStripMenuItem,
+            this.gestãoDeProcessosToolStripMenuItem,
+            this.gestãoDeProjetosToolStripMenuItem,
+            this.gestãoDePareceresToolStripMenuItem,
+            this.gestãoDeFuncionáriosToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Size = new System.Drawing.Size(50, 20);
+            this.toolStripMenu.Text = "Menu";
+            // 
+            // gestãoDePromotoresToolStripMenuItem
+            // 
+            this.gestãoDePromotoresToolStripMenuItem.Name = "gestãoDePromotoresToolStripMenuItem";
+            this.gestãoDePromotoresToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.gestãoDePromotoresToolStripMenuItem.Text = "Gestão de Promotores";
+            this.gestãoDePromotoresToolStripMenuItem.Click += new System.EventHandler(this.gestãoDePromotoresToolStripMenuItem_Click);
+            // 
+            // gestãoDeProcessosToolStripMenuItem
+            // 
+            this.gestãoDeProcessosToolStripMenuItem.Name = "gestãoDeProcessosToolStripMenuItem";
+            this.gestãoDeProcessosToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.gestãoDeProcessosToolStripMenuItem.Text = "Gestão de Processos";
+            this.gestãoDeProcessosToolStripMenuItem.Click += new System.EventHandler(this.gestãoDeProcessosToolStripMenuItem_Click);
+            // 
+            // gestãoDeProjetosToolStripMenuItem
+            // 
+            this.gestãoDeProjetosToolStripMenuItem.Name = "gestãoDeProjetosToolStripMenuItem";
+            this.gestãoDeProjetosToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.gestãoDeProjetosToolStripMenuItem.Text = "Gestão de Projetos";
+            this.gestãoDeProjetosToolStripMenuItem.Click += new System.EventHandler(this.gestãoDeProjetosToolStripMenuItem_Click);
+            // 
+            // gestãoDePareceresToolStripMenuItem
+            // 
+            this.gestãoDePareceresToolStripMenuItem.Name = "gestãoDePareceresToolStripMenuItem";
+            this.gestãoDePareceresToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.gestãoDePareceresToolStripMenuItem.Text = "Gestão de Pareceres";
+            this.gestãoDePareceresToolStripMenuItem.Click += new System.EventHandler(this.gestãoDePareceresToolStripMenuItem_Click);
+            // 
+            // gestãoDeFuncionáriosToolStripMenuItem
+            // 
+            this.gestãoDeFuncionáriosToolStripMenuItem.Name = "gestãoDeFuncionáriosToolStripMenuItem";
+            this.gestãoDeFuncionáriosToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.gestãoDeFuncionáriosToolStripMenuItem.Text = "Gestão de Funcionários";
+            this.gestãoDeFuncionáriosToolStripMenuItem.Click += new System.EventHandler(this.gestãoDeFuncionáriosToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(50, 79);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(433, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(538, 247);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestão de Projetos da Câmara Municipal";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem gestãoDePromotoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestãoDeProcessosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestãoDeProjetosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestãoDePareceresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestãoDeFuncionáriosToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
+
---
+++ app/GestaoCamaraMunicipal/GestaoCamaraMunicipal/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestaoCamaraMunicipal
+{
+    public partial class Form1 : Form
+    {
+        private GestaoCamaraMunicipalContainer camaraMunicipal;
+        public Form1()
+        {
+            InitializeComponent();
+            camaraMunicipal = new GestaoCamaraMunicipalContainer();
+
+        }
+
+        private void gestãoDePromotoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GestaoPromotores form = new GestaoPromotores();
+            form.Show();
+            this.Hide();
+        }
+
+        private void gestãoDeProcessosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GestaoProcessos form = new GestaoProcessos();
+            form.Show();
+            this.Hide();
+        }
+
+        private void gestãoDeProjetosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GestaoProjetos form = new GestaoProjetos();
+            form.Show();
+            this.Hide();
+        }
+
+        private void gestãoDePareceresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GestaoPareceres form = new GestaoPareceres();
+            form.Show();
+            this.Hide();
+        }
+
+        private void gestãoDeFuncionáriosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GestaoFuncionarios form = new GestaoFuncionarios();
+            form.Show();
+            this.Hide();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Sair() == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Sair() == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private DialogResult Sair()
+        {
+            DialogResult resposta = MessageBox.Show("Tem a certeza que deseja sair?", "Sair", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            return resposta;
+        }
+    }
+}
