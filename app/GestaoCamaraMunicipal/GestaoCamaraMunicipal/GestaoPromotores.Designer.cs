e7d47897e026986d8f6288198310476b8771294b
Goncalo <2201131@my.ipleiria.pt>
2021-05-05 15:11:04 +0100

    Form Promtores
    feitinhoooopo

--- app/GestaoCamaraMunicipal/GestaoCamaraMunicipal/GestaoPromotores.Designer.cs
+++ app/GestaoCamaraMunicipal/GestaoCamaraMunicipal/GestaoPromotores.Designer.cs
@@ -33,6 +33,25 @@ namespace GestaoCamaraMunicipal
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStripMenu = new System.Windows.Forms.ToolStripMenuItem();
             this.gestãoDePromotoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBoxPromotores = new System.Windows.Forms.ListBox();
+            this.lblPromotores = new System.Windows.Forms.Label();
+            this.lblNIF = new System.Windows.Forms.Label();
+            this.maskedTextBoxNIF = new System.Windows.Forms.MaskedTextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtBoxNome = new System.Windows.Forms.TextBox();
+            this.txtBoxMorada = new System.Windows.Forms.TextBox();
+            this.lblMorada = new System.Windows.Forms.Label();
+            this.txtBoxTelemovel = new System.Windows.Forms.TextBox();
+            this.lblTelemovel = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.maskedTextBoxMail = new System.Windows.Forms.MaskedTextBox();
+            this.lblCodAcesso = new System.Windows.Forms.Label();
+            this.txtBoxCodAcesso = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtBoxPassword = new System.Windows.Forms.TextBox();
+            this.btnRegistar = new System.Windows.Forms.Button();
+            this.btnGuardarAlteracoes = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,8 +63,8 @@ namespace GestaoCamaraMunicipal
             this.toolStripMenu});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(600, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(967, 28);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -54,25 +73,208 @@ namespace GestaoCamaraMunicipal
             this.toolStripMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.gestãoDePromotoresToolStripMenuItem});
             this.toolStripMenu.Name = "toolStripMenu";
-            this.toolStripMenu.Size = new System.Drawing.Size(50, 20);
+            this.toolStripMenu.Size = new System.Drawing.Size(60, 24);
             this.toolStripMenu.Text = "Menu";
             // 
             // gestãoDePromotoresToolStripMenuItem
             // 
             this.gestãoDePromotoresToolStripMenuItem.Name = "gestãoDePromotoresToolStripMenuItem";
-            this.gestãoDePromotoresToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.gestãoDePromotoresToolStripMenuItem.Size = new System.Drawing.Size(193, 26);
             this.gestãoDePromotoresToolStripMenuItem.Text = "Voltar ao Menu";
             this.gestãoDePromotoresToolStripMenuItem.Click += new System.EventHandler(this.gestãoDePromotoresToolStripMenuItem_Click);
             // 
+            // listBoxPromotores
+            // 
+            this.listBoxPromotores.FormattingEnabled = true;
+            this.listBoxPromotores.ItemHeight = 16;
+            this.listBoxPromotores.Location = new System.Drawing.Point(32, 62);
+            this.listBoxPromotores.Name = "listBoxPromotores";
+            this.listBoxPromotores.Size = new System.Drawing.Size(285, 404);
+            this.listBoxPromotores.TabIndex = 2;
+            this.listBoxPromotores.SelectedIndexChanged += new System.EventHandler(this.listBoxPromotores_SelectedIndexChanged);
+            // 
+            // lblPromotores
+            // 
+            this.lblPromotores.AutoSize = true;
+            this.lblPromotores.Location = new System.Drawing.Point(29, 42);
+            this.lblPromotores.Name = "lblPromotores";
+            this.lblPromotores.Size = new System.Drawing.Size(81, 17);
+            this.lblPromotores.TabIndex = 3;
+            this.lblPromotores.Text = "Promotores";
+            // 
+            // lblNIF
+            // 
+            this.lblNIF.AutoSize = true;
+            this.lblNIF.Location = new System.Drawing.Point(342, 68);
+            this.lblNIF.Name = "lblNIF";
+            this.lblNIF.Size = new System.Drawing.Size(29, 17);
+            this.lblNIF.TabIndex = 4;
+            this.lblNIF.Text = "NIF";
+            // 
+            // maskedTextBoxNIF
+            // 
+            this.maskedTextBoxNIF.Location = new System.Drawing.Point(345, 88);
+            this.maskedTextBoxNIF.Mask = "999999999";
+            this.maskedTextBoxNIF.Name = "maskedTextBoxNIF";
+            this.maskedTextBoxNIF.Size = new System.Drawing.Size(81, 22);
+            this.maskedTextBoxNIF.TabIndex = 5;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(342, 119);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(45, 17);
+            this.lblNome.TabIndex = 6;
+            this.lblNome.Text = "Nome";
+            // 
+            // txtBoxNome
+            // 
+            this.txtBoxNome.Location = new System.Drawing.Point(345, 139);
+            this.txtBoxNome.Name = "txtBoxNome";
+            this.txtBoxNome.Size = new System.Drawing.Size(274, 22);
+            this.txtBoxNome.TabIndex = 7;
+            // 
+            // txtBoxMorada
+            // 
+            this.txtBoxMorada.Location = new System.Drawing.Point(345, 193);
+            this.txtBoxMorada.Name = "txtBoxMorada";
+            this.txtBoxMorada.Size = new System.Drawing.Size(274, 22);
+            this.txtBoxMorada.TabIndex = 9;
+            // 
+            // lblMorada
+            // 
+            this.lblMorada.AutoSize = true;
+            this.lblMorada.Location = new System.Drawing.Point(344, 173);
+            this.lblMorada.Name = "lblMorada";
+            this.lblMorada.Size = new System.Drawing.Size(56, 17);
+            this.lblMorada.TabIndex = 8;
+            this.lblMorada.Text = "Morada";
+            // 
+            // txtBoxTelemovel
+            // 
+            this.txtBoxTelemovel.Location = new System.Drawing.Point(345, 247);
+            this.txtBoxTelemovel.Name = "txtBoxTelemovel";
+            this.txtBoxTelemovel.Size = new System.Drawing.Size(142, 22);
+            this.txtBoxTelemovel.TabIndex = 11;
+            // 
+            // lblTelemovel
+            // 
+            this.lblTelemovel.AutoSize = true;
+            this.lblTelemovel.Location = new System.Drawing.Point(346, 227);
+            this.lblTelemovel.Name = "lblTelemovel";
+            this.lblTelemovel.Size = new System.Drawing.Size(73, 17);
+            this.lblTelemovel.TabIndex = 10;
+            this.lblTelemovel.Text = "Telemovel";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(348, 285);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(47, 17);
+            this.lblEmail.TabIndex = 12;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // maskedTextBoxMail
+            // 
+            this.maskedTextBoxMail.Location = new System.Drawing.Point(347, 305);
+            this.maskedTextBoxMail.Mask = "aaaaaaaaaaaaaaaaaa@aaaaaaaaa.aaa";
+            this.maskedTextBoxMail.Name = "maskedTextBoxMail";
+            this.maskedTextBoxMail.Size = new System.Drawing.Size(267, 22);
+            this.maskedTextBoxMail.TabIndex = 13;
+            // 
+            // lblCodAcesso
+            // 
+            this.lblCodAcesso.AutoSize = true;
+            this.lblCodAcesso.Location = new System.Drawing.Point(342, 339);
+            this.lblCodAcesso.Name = "lblCodAcesso";
+            this.lblCodAcesso.Size = new System.Drawing.Size(122, 17);
+            this.lblCodAcesso.TabIndex = 14;
+            this.lblCodAcesso.Text = "Código de Acesso";
+            // 
+            // txtBoxCodAcesso
+            // 
+            this.txtBoxCodAcesso.Location = new System.Drawing.Point(345, 359);
+            this.txtBoxCodAcesso.Name = "txtBoxCodAcesso";
+            this.txtBoxCodAcesso.Size = new System.Drawing.Size(142, 22);
+            this.txtBoxCodAcesso.TabIndex = 15;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(342, 393);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(69, 17);
+            this.lblPassword.TabIndex = 16;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtBoxPassword
+            // 
+            this.txtBoxPassword.Location = new System.Drawing.Point(345, 413);
+            this.txtBoxPassword.Name = "txtBoxPassword";
+            this.txtBoxPassword.Size = new System.Drawing.Size(170, 22);
+            this.txtBoxPassword.TabIndex = 17;
+            this.txtBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // btnRegistar
+            // 
+            this.btnRegistar.Location = new System.Drawing.Point(345, 447);
+            this.btnRegistar.Name = "btnRegistar";
+            this.btnRegistar.Size = new System.Drawing.Size(136, 29);
+            this.btnRegistar.TabIndex = 18;
+            this.btnRegistar.Text = "Registar";
+            this.btnRegistar.UseVisualStyleBackColor = true;
+            this.btnRegistar.Click += new System.EventHandler(this.btnRegistar_Click);
+            // 
+            // btnGuardarAlteracoes
+            // 
+            this.btnGuardarAlteracoes.Location = new System.Drawing.Point(502, 447);
+            this.btnGuardarAlteracoes.Name = "btnGuardarAlteracoes";
+            this.btnGuardarAlteracoes.Size = new System.Drawing.Size(164, 29);
+            this.btnGuardarAlteracoes.TabIndex = 19;
+            this.btnGuardarAlteracoes.Text = "Guardar Alterações";
+            this.btnGuardarAlteracoes.UseVisualStyleBackColor = true;
+            this.btnGuardarAlteracoes.Click += new System.EventHandler(this.btnGuardarAlteracoes_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(686, 447);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(136, 29);
+            this.btnRemover.TabIndex = 20;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
             // GestaoPromotores
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(967, 488);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnGuardarAlteracoes);
+            this.Controls.Add(this.btnRegistar);
+            this.Controls.Add(this.txtBoxPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtBoxCodAcesso);
+            this.Controls.Add(this.lblCodAcesso);
+            this.Controls.Add(this.maskedTextBoxMail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtBoxTelemovel);
+            this.Controls.Add(this.lblTelemovel);
+            this.Controls.Add(this.txtBoxMorada);
+            this.Controls.Add(this.lblMorada);
+            this.Controls.Add(this.txtBoxNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.maskedTextBoxNIF);
+            this.Controls.Add(this.lblNIF);
+            this.Controls.Add(this.lblPromotores);
+            this.Controls.Add(this.listBoxPromotores);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "GestaoPromotores";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gestão de Promotores";
@@ -89,5 +291,24 @@ namespace GestaoCamaraMunicipal
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenu;
         private System.Windows.Forms.ToolStripMenuItem gestãoDePromotoresToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBoxPromotores;
+        private System.Windows.Forms.Label lblPromotores;
+        private System.Windows.Forms.Label lblNIF;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxNIF;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtBoxNome;
+        private System.Windows.Forms.TextBox txtBoxMorada;
+        private System.Windows.Forms.Label lblMorada;
+        private System.Windows.Forms.TextBox txtBoxTelemovel;
+        private System.Windows.Forms.Label lblTelemovel;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxMail;
+        private System.Windows.Forms.Label lblCodAcesso;
+        private System.Windows.Forms.TextBox txtBoxCodAcesso;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtBoxPassword;
+        private System.Windows.Forms.Button btnRegistar;
+        private System.Windows.Forms.Button btnGuardarAlteracoes;
+        private System.Windows.Forms.Button btnRemover;
     }
 }
--- app/GestaoCamaraMunicipal/GestaoCamaraMunicipal/GestaoPromotores.cs
+++ app/GestaoCamaraMunicipal/GestaoCamaraMunicipal/GestaoPromotores.cs
@@ -13,9 +13,12 @@ namespace GestaoCamaraMunicipal
     public partial class GestaoPromotores : Form
     {
         Form1 formprincipal = new Form1();
+        private GestaoCamaraMunicipalContainer camaraMunicipal;
         public GestaoPromotores()
         {
             InitializeComponent();
+            camaraMunicipal = new GestaoCamaraMunicipalContainer();
+            LerDados();
         }
 
         private void gestãoDePromotoresToolStripMenuItem_Click(object sender, EventArgs e)
@@ -26,7 +29,87 @@ namespace GestaoCamaraMunicipal
 
         private void GestaoPromotores_FormClosing(object sender, FormClosingEventArgs e)
         {
+            camaraMunicipal.Dispose();
             formprincipal.Sair(e);
+        }
+
+        private void LerDados()
+        {
+            listBoxPromotores.DataSource = camaraMunicipal.PromotorSet.ToList<Promotor>();
+        }
+
+        private void btnRegistar_Click(object sender, EventArgs e)
+        {
+            camaraMunicipal.PromotorSet.Add(new Promotor(Int32.Parse(maskedTextBoxNIF.Text), txtBoxNome.Text, txtBoxMorada.Text, txtBoxTelemovel.Text, maskedTextBoxMail.Text, txtBoxCodAcesso.Text, txtBoxPassword.Text));
+            camaraMunicipal.SaveChanges();
+            LerDados();
+        }
+
+        private void btnGuardarAlteracoes_Click(object sender, EventArgs e)
+        {
+            Promotor promotor = new Promotor();
+            try
+            {
+                if (listBoxPromotores.SelectedItem != null)
+                {
+                    promotor = (Promotor)listBoxPromotores.SelectedItem;
+                    promotor.NIF = Int32.Parse(maskedTextBoxNIF.Text);
+                    promotor.Nome = txtBoxNome.Text;
+                    promotor.Morada = txtBoxMorada.Text;
+                    promotor.Telemovel = txtBoxTelemovel.Text;
+                    promotor.Email = maskedTextBoxMail.Text;
+                    promotor.CodigoAcesso = txtBoxCodAcesso.Text;
+                    promotor.Senha = txtBoxPassword.Text;
+                    camaraMunicipal.SaveChanges();
+                    LerDados();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro \n" + ex);
+            }
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            Promotor promotor = new Promotor();
+            try
+            {
+                if (listBoxPromotores.SelectedItem != null)
+                {
+                    promotor = (Promotor)listBoxPromotores.SelectedItem;
+                    camaraMunicipal.PromotorSet.Remove(promotor);
+                    camaraMunicipal.SaveChanges();
+                    LerDados();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro \n" + ex);
+            }
+        }
+
+        private void listBoxPromotores_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Promotor promotor = new Promotor();
+            try
+            {
+                if (listBoxPromotores.SelectedItem != null)
+                {
+                    promotor = (Promotor)listBoxPromotores.SelectedItem;
+                    maskedTextBoxNIF.Text = promotor.NIF.ToString();
+                    txtBoxNome.Text = promotor.Nome;
+                    txtBoxMorada.Text = promotor.Morada;
+                    txtBoxTelemovel.Text = promotor.Telemovel;
+                    maskedTextBoxMail.Text = promotor.Email;
+                    txtBoxCodAcesso.Text = promotor.CodigoAcesso;
+                    txtBoxPassword.Text = promotor.Senha;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro \n" + ex);
+            }
         }
     }
 }
