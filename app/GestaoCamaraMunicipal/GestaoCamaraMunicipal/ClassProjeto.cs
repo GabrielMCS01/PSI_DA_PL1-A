f7a8ffd949b24cba5ee6feb6c630cbcc458304fc
Diogo Pereira <diogo.rc.pereira@gmail.com>
2021-05-11 02:05:42 +0100

    class projetos

---
+++ app/GestaoCamaraMunicipal/GestaoCamaraMunicipal/ClassProjeto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoCamaraMunicipal
+{
+    public partial class Projeto
+    {
+        public Projeto(string estadoProjeto, DateTime DataAprovacao, DateTime DataEntrada, int processo)
+        {
+            this.Documento = new HashSet<Documento>();
+            this.Parecer = new HashSet<Parecer>();
+            this.ProjetoAtribuido = new HashSet<ProjetoAtribuido>();
+            this.EstadoProjeto = estadoProjeto;
+            this.DataAprovacao = DataAprovacao;
+            this.DataEntrada = DataEntrada;
+            this.ProcessoId = processo;
+        }
+    }
+}
