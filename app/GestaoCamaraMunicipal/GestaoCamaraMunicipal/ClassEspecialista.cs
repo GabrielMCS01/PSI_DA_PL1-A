88f6c0671e92e0371b799cfd66c3f3bd2d6da332
Diogo Pereira <diogo.rc.pereira@gmail.com>
2021-06-11 11:03:56 +0100

    alterações

--- app/GestaoCamaraMunicipal/GestaoCamaraMunicipal/ClassEspecialista.cs
+++ app/GestaoCamaraMunicipal/GestaoCamaraMunicipal/ClassEspecialista.cs
@@ -2,6 +2,17 @@
 {
     public partial class Especialista
     {
+        public Especialista()
+        {
+
+        }
+
+        public Especialista(Funcionario funcionario, TipoProjeto tipoprojeto)
+        {
+            this.Funcionario = funcionario;
+            this.TipoProjeto = tipoprojeto;
+        }
+
         public override string ToString()
         {
             return string.Format("{0}", Funcionario);
